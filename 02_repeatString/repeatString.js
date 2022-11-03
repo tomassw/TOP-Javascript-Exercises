645b6e094503882bc2dee1019019f9f7c990c561
tomassw <75063636+tomassw@users.noreply.github.com>
2022-11-03 18:14:23 +0200

    Solved 01 to 07 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
 
-};
+    let stringrepeat = "";
+    for (let i = 0; i < num; i++) {
+        stringrepeat += string;
+        console.log(stringrepeat);
+    }
+    
+    return stringrepeat;
+};    
 
-// Do not edit below this line
+// Do not edit below this linen
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let array = args[0];
 
+    let newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
