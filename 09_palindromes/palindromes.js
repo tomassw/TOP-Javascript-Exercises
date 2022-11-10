4e52d51d3a4d973f0b9dcea747d4f5bd8d33b074
tomassw <75063636+tomassw@users.noreply.github.com>
2022-11-10 14:31:43 +0200

    Solved fibonacci
    tests might be wrong? because not even the original solution doesn't work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let processedString = string.toLowerCase().replace(/[^a-z]/g, "");
 
+    return (
+        processedString.split("").reverses.join("") == processedString
+    )
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return 'OOPS';
+    if (index === 0) return 0;
+    if (index == 1 || 2) return 1;
+   
+    let actualNumber = 1;
+    let exNumber = 1;
+    let temp;
+    //1 1 2 3 5 
 
+
+    //1 -> 1
+    //2 -> 1
+    //3 -> 1 + 1
+    //4 -> 1 + 2
+    for (let i = 2; i <= index; i++) {
+        temp = actualNumber;
+        actualNumber += exNumber;
+        exNumber = temp;
+    }
+    return actualNumber;
 };
 
 // Do not edit below this line
